3e5b01c23a9f1d386a818fe5cbe7fb8c6c0a4fe9
cielartisan <cielartisan@localhost>
2012-06-17 02:39:35 +0900

    1.5.4.20617: sets "color:black" to option

--- ctouch_common/ctouch_css.js
+++ ctouch_common/ctouch_css.js
@@ -4,6 +4,8 @@ s.type = "text/javascript";
 s.id = "ctouch_css_js";
 s.innerText += "\
 document.getElementsByTagName('body')[0].style.overflow='visible';\
+var opt=document.getElementsByTagName('option');\
+if(opt)for(i=0;i<opt.length;i++)opt[i].style.color='black';\
 var myself = document.getElementById('ctouch_css_js');\
 myself.parentNode.removeChild(myself);";
 document.documentElement.appendChild(s);
--- support/genversion.rb
+++ support/genversion.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 
 T=Time.new
-V="1.5.3."+sprintf("%d%02d%02d",T.year%10,T.month,T.day)
+V="1.5.4."+sprintf("%d%02d%02d",T.year%10,T.month,T.day)
 
 def getver
 	print V
