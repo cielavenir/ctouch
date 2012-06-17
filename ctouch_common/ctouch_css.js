8040e64269330d257e33f2eba6118583eda45bb4
cielartisan <cielartisan@localhost>
2012-06-17 17:25:14 +0900

    1.5.5.20617: text is now selectable

--- ctouch_common/ctouch_css.js
+++ ctouch_common/ctouch_css.js
@@ -4,6 +4,7 @@ s.type = "text/javascript";
 s.id = "ctouch_css_js";
 s.innerText += "\
 document.getElementsByTagName('body')[0].style.overflow='visible';\
+document.getElementsByTagName('body')[0].style.webkitUserSelect='auto';\
 var opt=document.getElementsByTagName('option');\
 if(opt)for(i=0;i<opt.length;i++)opt[i].style.color='black';\
 var myself = document.getElementById('ctouch_css_js');\
