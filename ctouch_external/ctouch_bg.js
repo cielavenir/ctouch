bc77138f8c5433075404e253d59becba3049173f
cielartisan <cielartisan@localhost>
2012-09-16 03:50:08 +0900

    1.8.4.20916 Forces to load Reel-1.2.js

--- ctouch_external/ctouch_bg.js
+++ ctouch_external/ctouch_bg.js
@@ -80,3 +80,16 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 	},
 	['blocking', 'requestHeaders']
 );
+
+chrome.webRequest.onBeforeRequest.addListener(
+	function(details){
+		var url=details.url;
+		var reel_url='http://aimg.gree.jp/js/reel/Reel-1.2.js';
+		if(url!=reel_url&&url.substr(0,32)==reel_url.substr(0,32))return {redirectUrl: reel_url};
+	},
+	{
+		urls: ['<all_urls>'],
+		types: ['main_frame','sub_frame','stylesheet','script','image','object','xmlhttprequest','other']
+	},
+	['blocking']
+);
