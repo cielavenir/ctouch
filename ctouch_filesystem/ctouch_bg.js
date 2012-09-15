b96a8d56c88a3fcf709d0a01c2e8c2cc09039dd2
cielartisan <cielartisan@localhost>
2012-09-16 04:32:08 +0900

    something is wrong...

--- ctouch_filesystem/ctouch_bg.js
+++ ctouch_filesystem/ctouch_bg.js
@@ -84,8 +84,14 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 chrome.webRequest.onBeforeRequest.addListener(
 	function(details){
 		var url=details.url;
+		var reel02_url='http://aimg.gree.jp/js/reel/Reel.js';
+		var reel05_url='http://aimg.gree.jp/js/reel/Reel-0.5.js';
 		var reel_url='http://aimg.gree.jp/js/reel/Reel-1.2.js';
-		if(url!=reel_url&&url.substr(0,32)==reel_url.substr(0,32))return {redirectUrl: reel_url};
+		if(url!=reel_url&&(
+			//url.substr(0,32)==reel_url.substr(0,32) ||
+			url==reel02_url ||
+			url==reel05_url
+		))return {redirectUrl: reel_url};
 	},
 	{
 		urls: ['<all_urls>'],
