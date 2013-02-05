4de5ddfbd9bfef4e73f44c5be0e32f0af0e454cb
cielartisan <cielartisan@localhost>
2013-02-05 13:06:34 +0900

    Fixed safari.rb for xar on OSX.

--- support/safari.rb
+++ support/safari.rb
@@ -4,12 +4,12 @@
 XAR=ENV['HOME']+'/bin/xar'
 
 =begin
-#SIGLEN=`</dev/null openssl dgst -sign ctouch_safari.pem -binary | wc -c`
+#SIGLEN=`</dev/null openssl dgst -sign pem/ctouch_safari.pem -binary | wc -c`
 SIGLEN=256
-~/bin/xar -cf ctouch.safariextz ctouch.safariextension/
-~/bin/xar --sign -f ctouch.safariextz --data-to-sign sha1_hash.dat --sig-size $SIGLEN --cert-loc ctouch_safari.der --cert-loc certs/appleca --cert-loc certs/rootca
-(echo "3021300906052B0E03021A05000414" | xxd -r -p; cat sha1_hash.dat) |openssl rsautl -sign -inkey ctouch_safari.pem > signature.dat
-~/bin/xar --inject-sig signature.dat -f ctouch.safariextz
+~/bin/xar -cf bin/ctouch.safariextz ctouch.safariextension/
+~/bin/xar --sign -f bin/ctouch.safariextz --data-to-sign sha1_hash.dat --sig-size $SIGLEN --cert-loc pem/ctouch_safari.der --cert-loc certs/appleca --cert-loc certs/rootca
+(echo "3021300906052B0E03021A05000414" | xxd -r -p; cat sha1_hash.dat) |openssl rsautl -sign -inkey pem/ctouch_safari.pem > signature.dat
+~/bin/xar --inject-sig signature.dat -f bin/ctouch.safariextz
 rm -f sha1_hash.dat signature.dat
 =end
 
@@ -37,13 +37,19 @@ def run(argv)
 	end
 	siglen=key.sign(OpenSSL::Digest::SHA1.new,'').size
 
-	system(%Q("#{XAR}" -cf "bin/#{name}.safariextz" "#{name}.safariextension/"))
+	system(%Q("#{XAR}" -cf "bin/#{name}.safariextz" --compression=gzip --distribution "#{name}.safariextension/"))
 
 	#I'd like perform this kind of signature handling without using exe...
 	sha1=''
-	Open3.popen3(%Q("#{XAR}" --sign -f "bin/#{name}.safariextz" --data-to-sign /dev/stderr --sig-size #{siglen} --cert-loc #{pder} --cert-loc certs/appleca --cert-loc certs/rootca)){|stdin,stdout,stderr|
-		sha1=stderr.read
-	}
+	file = Tempfile.new('sha1')
+	file.close
+	system(%Q("#{XAR}" --sign -f "bin/#{name}.safariextz" --data-to-sign "#{file.path}" --sig-size #{siglen} --cert-loc #{pder} --cert-loc certs/appleca --cert-loc certs/rootca))
+	file.open
+	sha1=file.read
+	file.close!
+	#Open3.popen3(%Q("#{XAR}" --sign -f "bin/#{name}.safariextz" --data-to-sign /dev/stderr --sig-size #{siglen} --cert-loc #{pder} --cert-loc certs/appleca --cert-loc certs/rootca)){|stdin,stdout,stderr|
+	#	sha1=stderr.read
+	#}
 	signature=key.private_encrypt(KEY+sha1)
 
 	file = Tempfile.new('sign')
